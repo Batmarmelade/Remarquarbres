755e66265ae12567517911c529a59dda7b578b8f
Loowood <lorisg01@yahoo.fr>
2019-04-17 00:46:06 +0200

    Ajout de 5 Questions
    Question Botanique, Latitude / Longitude, Addresse et Observations ajoutées

--- app/src/main/java/com/esaip/remarquarbres/Quiz.java
+++ app/src/main/java/com/esaip/remarquarbres/Quiz.java
@@ -42,9 +42,51 @@ public class Quiz extends AppCompatActivity {
 
     public void submit(android.view.View v){
         Intent returnIntent = getIntent();
-        returnIntent.putExtra("Nom", ((EditText) findViewById(R.id.NomTxtEd)).getText().toString() );
+
+        // Nom & Prénom
+        returnIntent.putExtra("Nom & Prénom", ((EditText) findViewById(R.id.NomTxtEd)).getText().toString() );
+
+        // Pseudonyme
         returnIntent.putExtra("Pseudo", ((EditText) findViewById(R.id.PseudoTxtEd)).getText().toString() );
+
+        // Addresse Mail
         returnIntent.putExtra("Mail", ((EditText) findViewById(R.id.MailTxtEd)).getText().toString() );
+
+        // Nom de l'arbre
+        String strNomArbre = "";
+        RadioGroup choixArbresRG = (RadioGroup) findViewById(R.id.ArbresRG);
+        RadioButton rbArbreAutre = (RadioButton) findViewById(R.id.ArbresChoixAutreRb);
+        if ( rbArbreAutre.isChecked() ){
+            strNomArbre = "Autre, ";
+            strNomArbre = strNomArbre.concat(((EditText) findViewById(R.id.ArbresAutreEd)).getText().toString());
+        } else {
+            if ( choixArbresRG.getCheckedRadioButtonId() != -1 ) {
+                RadioButton rbChecked = (RadioButton) findViewById(choixArbresRG.getCheckedRadioButtonId());
+                strNomArbre = rbChecked.getText().toString();
+            }
+        }
+        returnIntent.putExtra("Nom de l'arbre", strNomArbre);
+
+        // Nom Botanique de l'arbre
+        returnIntent.putExtra("Nom Botanique", ((EditText) findViewById(R.id.BotaniqueTxtEd)).getText().toString() );
+
+        // Latitude
+        returnIntent.putExtra("Latitude", ((EditText) findViewById(R.id.LatitudeTxtEd)).getText().toString() );
+
+        // Longitude
+        returnIntent.putExtra("Longitude", ((EditText) findViewById(R.id.LongitudeTxtEd)).getText().toString() );
+
+        // Addresse
+        returnIntent.putExtra("Addresse", ((EditText) findViewById(R.id.AddresseTxtEd)).getText().toString() );
+
+        /*
+         *
+         * TO DO
+         *
+         */
+
+        // Observations
+        returnIntent.putExtra("Observations", ((EditText) findViewById(R.id.ObservationsTxtEd)).getText().toString() );
         setResult(Activity.RESULT_OK, returnIntent);
         finish();
     }
