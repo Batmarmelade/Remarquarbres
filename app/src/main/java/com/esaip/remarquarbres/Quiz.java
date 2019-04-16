0aa6a22583e55a865e2061cded398af3d0c0b0a6
Loowood <lorisg01@yahoo.fr>
2019-04-16 23:33:01 +0200

    Question choix des arbres ajoutée
    Radio Group

--- app/src/main/java/com/esaip/remarquarbres/Quiz.java
+++ app/src/main/java/com/esaip/remarquarbres/Quiz.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class Quiz extends AppCompatActivity {
 
@@ -15,6 +19,24 @@ public class Quiz extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         Log.d("D", "I'm in the Quiz");
+
+        RadioGroup choixArbresRG = (RadioGroup) findViewById(R.id.ArbresRG);
+        choixArbresRG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // checkedId is the RadioButton selected
+                EditText arbresAutreEd = (EditText) findViewById(R.id.ArbresAutreEd);
+                RadioButton rbArbreAutre = (RadioButton) findViewById(R.id.ArbresChoixAutreRb);
+                if (rbArbreAutre.isChecked()) {
+                    rbArbreAutre.setText(R.string.ArbreAutreSelected);
+                    arbresAutreEd.setVisibility(View.VISIBLE);
+                } else {
+                    rbArbreAutre.setText(R.string.ArbreAutre);
+                    arbresAutreEd.setVisibility(View.GONE);
+                }
+            }
+        });
 
     }
 
