3da767033944dfc4a75bc94a037f36290a705453
phenix04 <cypriantankeu@yahoo.fr>
2019-04-19 21:24:16 +0200

    Update AccueilActivity.java

--- app/src/main/java/com/esaip/remarquarbres/AccueilActivity.java
+++ app/src/main/java/com/esaip/remarquarbres/AccueilActivity.java
@@ -1,13 +1,62 @@
 package com.esaip.remarquarbres;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class AccueilActivity extends AppCompatActivity {
+
+    private ImageButton img;
+    private ImageButton mQuiz;
+    private ImageButton imap;
+    private ImageView mcopy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+        img = findViewById(R.id.img);
+        mQuiz = findViewById(R.id.mQuiz);
+        imap = findViewById(R.id.imap);
+        mcopy = findViewById(R.id.mCopy);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent testIntent = new Intent(AccueilActivity.this, Test.class );
+                startActivity(testIntent);
+            }
+        });
+
+        mQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent testIntent = new Intent(AccueilActivity.this, Test.class );
+                startActivity(testIntent);
+            }
+        });
+
+        imap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent testIntent = new Intent(AccueilActivity.this, Test.class );
+                startActivity(testIntent);
+            }
+        });
+
+        mcopy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent testIntent = new Intent(AccueilActivity.this, Test.class );
+                startActivity(testIntent);
+            }
+        });
+
     }
 }
