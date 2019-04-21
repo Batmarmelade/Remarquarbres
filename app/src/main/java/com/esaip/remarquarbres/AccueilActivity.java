9ce4d558307484b47244fb4b83a24079d8bb2da4
Mathieu <Mathieu.ponthoreau@hotmail.fr>
2019-04-21 21:35:19 +0200

    Auto stash before merge of "GPS" and "origin/GPS"

--- app/src/main/java/com/esaip/remarquarbres/AccueilActivity.java
+++ app/src/main/java/com/esaip/remarquarbres/AccueilActivity.java
@@ -1,7 +1,6 @@
 package com.esaip.remarquarbres;
 
 import android.content.Intent;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,28 +18,9 @@ public class AccueilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
-
-        Intent intent = new Intent(this, Quiz.class);
-        startActivityForResult(intent, code);
-
+        Intent intent = new Intent(this, GPS.class);
+        startActivityForResult(intent, 2);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if ( resultCode == RESULT_OK ){
-            Bundle bundle = data.getExtras();
-            if (bundle != null) {
-                Set<String> keys = bundle.keySet();
-                Iterator<String> it = keys.iterator();
-                Log.e("RESULTS","Dumping Intent start");
-                while (it.hasNext()) {
-                    String key = it.next();
-                    Log.e("RESULTS","[" + key + "=" + bundle.get(key)+"]");
-                    ((TextView) findViewById(R.id.DataDump)).append("\n[" + key + "=" + bundle.get(key)+"]");
-                }
-                Log.e("RESULTS","Dumping Intent end");
-            }
-        }
-    }
+
 }
---
+++ app/src/main/java/com/esaip/remarquarbres/GPS.java
@@ -0,0 +1,90 @@
+package com.esaip.remarquarbres;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.AlteredCharSequence;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+
+public class GPS extends AppCompatActivity {
+
+
+    private Button b;
+    private TextView t;
+    private LocationManager locationManager;
+    private LocationListener listener;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gps);
+
+        t = (TextView) findViewById(R.id.textView);
+        b = (Button) findViewById(R.id.button);
+
+        listener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                double longitude = location.getLongitude();
+                double latitude = location.getLatitude();
+                t.setText("Longitude : " + longitude + "\n" + "Latitude : " + latitude);
+            }
+
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            public void onProviderEnabled(String s) {
+
+            }
+
+            public void onProviderDisabled(String s) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(GPS.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(GPS.this,"en attente de la permission pour utiliser le GPS", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                                ,10);
+                    }
+                } else {
+                    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+                    if (isGPSEnabled) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 10, listener);
+                        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    } else {
+                        Toast.makeText(GPS.this, "Le GPS est désactivé", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+    }
+}
